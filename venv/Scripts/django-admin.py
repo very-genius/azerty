ab01434a0d7d594a27fded98669fcd3cd2067b3e
verygenius <verygenius@gmx.fr>
2018-08-29 23:25:34 +0200

    My Django Girls app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\alexandre\Desktop\secu\serveur\azerty\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
